1f2617ca8b0ca7dba932361e5a0306425bcc6000
PC Linux <e.borzechowski@gmail.com>
2023-08-03 11:37:17 +0200

    fix db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { User } from 'src/user/entities/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './typeorm.config';
 @Module({
   imports: [
     UserModule,
@@ -13,19 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     forwardRef(() =>
       TypeOrmModule.forRootAsync({
         imports: [ConfigModule],
-        useFactory: async (configService: ConfigService) => ({
-          type: 'mysql',
-          host: 'localhost',
-          port: 3306,
-          username: configService.get('MYSQL_USER'),
-          password: configService.get('MYSQL_PASS'),
-          database: configService.get('MYSQL_DB'),
-          logging: false,
-          entities: [User],
-          migrationsTableName: 'migrations',
-          migrations: ['dist/migration/*.js'],
-          subscribers: [],
-        }),
+        useFactory: async () => dataSourceOptions,
         inject: [ConfigService],
       }),
     ),
